28895eaae423bf43157dfbe0ed3d5b95fed4874b
Urfan00 <urfanagazade75@gmail.com>
2024-05-24 20:48:59 +0400

    add all file and folder

---
+++ back/main.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+for i in range(n):
+  a, b = map(str, input().split())
+  if float(a) >= 3.5 and (b == 'A' or b == 'B'):
+    print(1)
+  else:
+    print(0)
+
+# asdfkjdf  
+# dasfas fd
